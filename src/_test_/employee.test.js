8cda8c9e78301d2388581037fbb66a152f587058
Brenda  Sosa <brenda.sosa@gmail.com>
2022-08-18 11:34:36 -0400

    added the employee test

--- src/_test_/employee.test.js
+++ src/_test_/employee.test.js
@@ -1,3 +1,5 @@
+const Employee = require('../lib/employee');
+
 // Initialize testing
 
 describe("Employee", () =>{
@@ -8,3 +10,50 @@ describe("Employee", () =>{
     });
 });
 
+//test for name object
+
+describe("Name", () => {
+it("should create a new name", () => {
+    const obj = new Employee("Maria");
+    expect("obj.name").toEqual("Maria");
+
+});
+});
+
+// test id object
+
+describe("id", () => {
+    it("should create a new id" , () => {
+        const obj = new Employee("Maria", 12);
+        expect("obj.id").toEqual("12");
+
+    });
+});
+
+//test email object
+
+describe("getEmail", () => {
+  it("should create a new email", () => {
+    const obj = new Employee("Maria", 12,  "maria@gmail.com");
+    expect("obj.getEmail").toEqual("maria@gmail.com");
+  });
+});
+
+//test telephone object
+
+describe("getTelephone", () => {
+    it("should create a new telephone" , () => {
+        const obj = new Employee("Maria", 12,  305);
+        expect("obj.getTelephone").toEqual("305");
+
+    });
+});
+
+// test role
+describe("getRole", () => {
+    it("return role", () =>{
+    const role = "Employee";
+    const obj = new Employee("Maria" , 12 , 305 , "maria@gmail.com");
+    expect(obj.getRole()).toEqual(role);
+    });
+});
